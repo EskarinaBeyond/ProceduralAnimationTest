e1a592e7f7d25505bf037cdc0c85d991055f8330
Jonas Ulrich <sanoj.koch@web.de>
2019-12-28 09:29:26 +0100

    initial commit

---
+++ Scripts/Leg.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+onready var skel = $Leg_Skeleton/Skeleton
+var id
+
+func _ready():
+	id = skel.find_bone("Test")
+	set_process(true)
+
+func _process(delta):
+	var t = skel.get_bone_pose(id)
+	t = t.rotated(Vector3(0.0, 1.0, 0.0), 0.9 * delta)
+	skel.set_bone_pose(id, t)
